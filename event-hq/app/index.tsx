390d528e6a37a06989200a178102739265b76567
Quymbee Chen <quymbeechen@Quymbees-MacBook-Pro.local>
2026-01-14 01:51:46 -0500

    FlatList example

--- event-hq/app/index.tsx
+++ event-hq/app/index.tsx
@@ -1,23 +1,92 @@
-import { StyleSheet, Text, View , Button} from 'react-native';
+import { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  Button,
+  FlatList,
+} from "react-native";
 
 export default function App() {
+  const [enteredGoal, setEnteredGoal] = useState("");
+  const [goals, setGoals] = useState<{ text: string; id: string }[]>([]);
+
+  function goalInputHandler(e: string) {
+    setEnteredGoal(e);
+  }
+
+  function addGoalHandler() {
+    setGoals((prev) => [
+      ...prev,
+      { text: enteredGoal, id: Math.random().toString() },
+    ]);
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Hello World!!!!</Text>
-      <Text style={styles.dummyText}>Hello</Text>
-      <Button title="Tap me"  />
+    <View style={styles.appContainer}>
+      <View style={styles.inputContainer}>
+        <TextInput
+          placeholder="Your course goal"
+          style={styles.textInput}
+          onChangeText={goalInputHandler}
+        />
+        <Button title="Add Goal" onPress={addGoalHandler} />
+      </View>
+      <View style={styles.goalsContainer}>
+        <Text>List of Goals</Text>
+        {/* Using FlatList for better performance with large lists over <ScrollView> */}
+        <FlatList
+          data={goals}
+          alwaysBounceVertical={true}
+          keyExtractor={(item) => item.id}
+          renderItem={({ item }) => {
+            return (
+              <View style={styles.goalItem}>
+                <Text style={styles.goalText}>{item.text}</Text>
+              </View>
+            );
+          }}
+        />
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  appContainer: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: 50,
+    paddingHorizontal: 16,
   },
-  dummyText:{
-    margin: 16, borderWidth: 1, borderColor: 'green', padding: 16
-  }
+  inputContainer: {
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginBottom: 24,
+    borderBottomWidth: 1,
+    borderColor: "#cccccc",
+  },
+  textInput: {
+    flex: 1,
+    borderColor: "#cccccc",
+    borderWidth: 1,
+    width: "70%",
+    marginRight: 8,
+    padding: 8,
+  },
+  goalsContainer: {
+    flex: 5,
+  },
+  goalItem: {
+    marginVertical: 4,
+    borderRadius: 6,
+    backgroundColor: "pink",
+    padding: 8,
+  },
+  goalText: {
+    color: "white",
+    fontWeight: "bold",
+  },
 });
